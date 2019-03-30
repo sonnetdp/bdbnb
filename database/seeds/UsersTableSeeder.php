0225cf7b01287819999737edcb786f2c1dfdaecd
Sonnet <sonnetdp@gmail.com>
2019-03-30 16:41:54 +0600

    auth done

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = 'Super Admin';
+        $user->email = 'super@admin.com';
+        $user->password = bcrypt('admin123');
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('admin123');
+        $user->save();
+    }
+}
