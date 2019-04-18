0a190f9381219af348d5870045feb441087e3e8c
Sonnet <sonnetdp@gmail.com>
2019-04-18 16:47:30 +0600

    Seeder field updated

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,10 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Admin';
         $user->email = 'super@admin.com';
         $user->role = 1;
+        $user->address = 'Dhaka';
+        $user->contact = '+8801711934691';
+        $user->national_id = '191912124545878';
+        $user->passport_id = 'V5017895';
         $user->password = bcrypt('admin123');
         $user->save();
 
@@ -23,6 +27,10 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Manager';
         $user->email = 'manager@admin.com';
         $user->role = 2;
+        $user->address = 'Dhaka';
+        $user->contact = '+8801711420420';
+        $user->national_id = '1232132132132';
+        $user->passport_id = 'Z5017895';
         $user->password = bcrypt('admin123');
         $user->save();
     }
