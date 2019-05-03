b46c53963f5f19d3d1e1e067a497594b0fd3c846
rabbee <abdulgonirabbee@gmail.com>
2019-05-03 22:45:19 +0600

    organizing in header and footer

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -10,58 +10,58 @@
 <!-- End WOWSlider.com HEAD section -->
 	<title>Air Bdbnb</title>
 	<link rel="shortcut icon" href="{{url('images/ce-travel.jpg')}}">
+
 	<link rel="stylesheet" href="{{url('css/bootstrap.css')}}">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
 	<link rel="stylesheet" href="{{url('css/skincss.css')}}">
-	<script src="{{url('js/jquery.js')}}"></script>
-	<script src="{{url('js/bootstrap.js')}}"></script>
 </head>
 <body>
 
-	<nav class="navbar navbar-inverse navbar-static" style="border:none;">
 
-		<div class="col-md-1">
-			<div class="logo">
-				<a href=""><img src="{{url('images/402955_thumb.png')}}" alt="" class="img-responsive" width="100"></a>
-			</div>
-		</div>
-	<div class="col-md-8" style="padding-top: 50px; padding-bottom: 20px;">
-		<div class="navbar-header">
-			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-		</div>
-		<div class="collapse navbar-collapse js-navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li class="dropdown dropdown-large">
-					<a href="{{url('/')}}" style="color:#FFFFFF;">Home</a>
-				</li>
-				<li class="dropdown dropdown-large">
-					<a href="expriences.html" style="color: #FFFFFF;">Host and Exprience</a>
-				</li>
-				<li class="dropdown dropdown-large">
-					<a href="#" style="color:#FFFFFF;">Help</a>
-				</li>
-				<li>
-					<a href="{{url('/showFlat')}}">All flat</a>
-				</li>
-				<li>
-					<a href="{{url('/user/addFlat')}}" >Add Flat</a>
-				</li>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a href=""><img src="{{url('images/402955_thumb.png')}}" alt="" class="img-responsive" width="100"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+  </button>
 
-			</ul>
-		</div>
-	</div>
-	<div class="col-md-3" style="padding:15px; padding-left: 40px;">
-		<div class="container">
-			<a href="{{ route('login') }}" class="login_icone">
-          		<span class="glyphicon glyphicon-user">Login</span>
-        	</a>
-		</div>
-	</div>
- </div>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+  		<a class="nav-link" href="{{url('/')}}">Home</a>
+  	  </li>
+
+  	  <li class="nav-item">
+  		<a class="nav-link" href="#">Host and Exprience</a>
+  	  </li>
+
+  	  <li class="nav-item">
+  		<a class="nav-link" href="{{url('/')}}">Help</a>
+  	  </li>
+
+  	  <li class="nav-item">
+  		<a class="nav-link" href="{{url('/showFlat')}}">All flat</a>
+  	  </li>
+
+  	  <li class="nav-item">
+  		<a class="nav-link" href="{{url('/user/addFlat')}}">Add Flat</a>
+  	  </li>
+    </ul>
+
+	<ul class="navbar-nav ">
+		<li class="nav-item">
+			<a class="nav-link" href="{{ route('login') }}"><i class="far fa-user"></i> Login</a>
+		</li>
+	</ul>
+  </div>
 </nav>
+
+
+
+
+
 <!-- /.nav-collapse -->
 <section class="content">
 	@yield('index_body')
@@ -69,57 +69,67 @@
 
 <!-- footer......... -->
 <div class="footer">
+	<div class="container-fluid">
 		<div class="row">
+			<div class="col-md-3">
+				<ul>
+					<h4>Airbnb</h4>
+					<li><a href="#">Careers</a></li>
+					<li><a href="#">Press</a></li>
+					<li><a href="#">Policies</a></li>
+					<li><a href="#">Help</a></li>
+					<li><a href="#">Diversity&Belonging</a></li>
+				</ul>
+			</div>
 
 			<div class="col-md-3">
-						<ul class="footer_box" style="padding-left: 50px;">
-							<h4>Airbnb</h4>
-							<li><a href="#">Careers</a></li>
-							<li><a href="#">Press</a></li>
-							<li><a href="#">Policies</a></li>
-							<li><a href="#">Help</a></li>
-							<li><a href="#">Diversity&Belonging</a></li>
-						</ul>
-					</div>
+				<ul>
+					<h4>Discover</h4>
+					<li><a href="#">Trust & Safety</a></li>
+					<li><a href="#">Gift Cards</a></li>
+					<li><a href="#">Airbnb Citizen</a></li>
+					<li><a href="#">Business Travel</a></li>
+					<li><a href="#">Gidebooks</a></li>
+					<li><a href="#">Airbnbmag</a></li>
+					<li><a href="#">Events</a></li>
+				</ul>
+			</div>
 
-					<div class="col-md-3">
-						<ul class="footer_box">
-							<h4>Discover</h4>
-							<li><a href="#">Trust & Safety</a></li>
-							<li><a href="#">Gift Cards</a></li>
-							<li><a href="#">Airbnb Citizen</a></li>
-							<li><a href="#">Business Travel</a></li>
-							<li><a href="#">Gidebooks</a></li>
-							<li><a href="#">Airbnbmag</a></li>
-							<li><a href="#">Events</a></li>
-						</ul>
-					</div>
-					<div class="col-md-3">
-						<ul class="footer_box">
-							<h4>Hosting</h4>
-							<li><a href="#">Why Host</a></li>
-							<li><a href="#">Hospititality</a></li>
-							<li><a href="#">Responsible</a></li>
-							<li><a href="#">Hosting</a></li>
-							<li><a href="#">Community</a></li>
-							<li><a href="#">Center</a></li>
-							<li><a href="#">Open Homes</a></li>
-						</ul>
-					</div>
-					<div class="col-md-3">
-						<ul class="footer_box">
-							<h4>Newsletter</h4>
-							<div class="footer_search">
-				    		   <form action="" method="">
-				    			<input type="text" value="" placeholder="search">
-				    			<input type="submit" value="Go">
-				    		   </form>
-					        </div>
-						</ul>
-					<p style="color:#08EDCF;">© 2019 Travel.com <a href="#" target="_blank">SUMON MITRA</a></p>
+			<div class="col-md-3">
+				<ul>
+					<h4>Hosting</h4>
+					<li><a href="#">Why Host</a></li>
+					<li><a href="#">Hospititality</a></li>
+					<li><a href="#">Responsible</a></li>
+					<li><a href="#">Hosting</a></li>
+					<li><a href="#">Community</a></li>
+					<li><a href="#">Center</a></li>
+					<li><a href="#">Open Homes</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-3">
+				<ul>
+				<h4>Newsletter</h4>
+				<div class="newsletter">
+					<form class="form-inline" action="#" method="post">
+						<div class="form-group mb-2">
+							<input type="email" class="form-control" placeholder="Email address">
+						</div>
+						<input type="submit" class="btn btn-primary btn-sm mb-2" name="submit" value="Submit">
+					</form>
+				</div>
+				</ul>
 			</div>
 		</div>
+		<div class="row text-center">
+			<p>&copy; All rights reserved | 2019</p>
+		</div>
 	</div>
+</div>
 
+	<script src="{{url('js/jquery.js')}}"></script>
+	<script src="{{url('js/proper.js')}}"></script>
+	<script src="{{url('js/bootstrap.js')}}"></script>
 </body>
 </html>
